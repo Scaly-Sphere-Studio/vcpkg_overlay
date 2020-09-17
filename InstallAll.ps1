98e1c138633f761930a1b0bd9e025efa2318cbb0
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2020-09-17 19:21:19 +0200

    Old pkgs are removed before being installed. Update assets.json

--- InstallAll.ps1
+++ InstallAll.ps1
@@ -9,9 +9,13 @@ $assets = Get-Content -Path assets.json | ConvertFrom-Json;
 # Download and unzip all assets
 $assets | %{
     # Download
-    Download-Asset $_ $token $dl_dir -ErrorVariable err;
+    Download-Asset $_ $token $dl_dir;
+    # 
+    if (Test-Path "$ports_dir\$_.pkgname") {
+        Remove-Item -Force -Recurse "$ports_dir\$_.pkgname";
+    }
     # Unzip
-    Expand-Archive -Force -Path $dl_dir\$($_.filename) -DestinationPath $ports_dir -ErrorVariable err;
+    Expand-Archive -Force -Path $dl_dir\$($_.filename) -DestinationPath $ports_dir;
 }
 
 # Install via vcpkg
