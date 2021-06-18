a8fff2aa5c0c66455272e687b17a5d4b4b691f99
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2021-06-18 07:45:14 +0200

    UpgradeVcpkg now rebuilds vcpkg.exe
    This avoids things like libs being removed as vcpkg.exe's old versions may allow removal but not updation.

--- UpgradeVcpkg.ps1
+++ UpgradeVcpkg.ps1
@@ -1,4 +1,15 @@
-Write-Output "Pulling vcpkg's repository ...";
-git -C C:\dev\vcpkg\ pull;
-Write-Output "Upgrading vcpkg's packages ...";
+$ErrorActionPreference = "Stop";
+
+$vcpkg_dir = ($env:path).Split(";") | ?{ $_ -like "*\vcpkg" };
+if (!(Test-Path $vcpkg_dir)) {
+    Write-Error "Could not find vcpkg from PATH environment variable.";
+}
+
+Write-Host "> Pulling vcpkg's repository ...";
+git -C $vcpkg_dir pull;
+
+Write-Host "`n> Rebuilding vcpkg.exe ...";
+CMD /c $vcpkg_dir\bootstrap-vcpkg.bat
+
+Write-Host "`n> Upgrading vcpkg's packages ...";
 vcpkg upgrade --no-dry-run;
