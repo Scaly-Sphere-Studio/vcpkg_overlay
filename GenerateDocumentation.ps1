cca52eb025e36f70e5f7f1c9a4094665bbbd4c55
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-04-23 20:07:06 +0200

    Update GenerateDocumentation.ps1

--- GenerateDocumentation.ps1
+++ GenerateDocumentation.ps1
@@ -1,11 +1,21 @@
+$ErrorActionPreference = "Stop";
+
 # Clean previous doc files
-Remove-Item -Recurse -Force "$PSScriptRoot/doxygen/*";
+$old_doc = "$PSScriptRoot/doxygen/*";
+if (Test-Path $old_doc) {
+    Remove-Item -Recurse -Force $old_doc;
+}
+$shortcut_path = Join-Path $PSScriptRoot "Documentation.lnk";
+if (Test-Path $shortcut_path) {
+    Remove-Item -Force $shortcut_path;
+}
 # Generate doc
-doxygen.exe;
+$(type "$PSScriptRoot/Doxyfile"; echo "OUTPUT_DIRECTORY=$PSScriptRoot/doxygen") | doxygen.exe -
 # Create shortcut to index.html
-$SourceFilePath = Resolve-Path "$PSScriptRoot\doxygen\html\index.html";
-$ShortcutPath = "$PSScriptRoot\Documentation.lnk";
-$WScriptObj = New-Object -ComObject ("WScript.Shell");
-$shortcut = $WscriptObj.CreateShortcut($ShortcutPath);
-$shortcut.TargetPath = $SourceFilePath;
-$shortcut.Save();
+$index_path = Join-Path $PSScriptRoot "doxygen\html\index.html";
+if (Test-Path $index_path) {
+    $WScript_obj = New-Object -ComObject ("WScript.Shell");
+    $shortcut = $WScript_obj.CreateShortcut($shortcut_path);
+    $shortcut.TargetPath = $index_path;
+    $shortcut.Save();
+}
